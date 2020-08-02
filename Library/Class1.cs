2159998f649b29ef098c04d2d23fd0719cd35753
Brett Story <brettmstory@gmail.com>
2020-08-02 08:23:52 -0500

    Project shell created

---
+++ Library/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Zvukosti.Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tuner/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Macabresoft.Zvukosti.Tuner {
+
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+            this.InitializeComponent();
+        }
+    }
+}
