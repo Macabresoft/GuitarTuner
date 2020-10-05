050c9bb79cdcb79ece0b44fb7fd6f10b09494ffd
Brett Story <brettmstory@gmail.com>
2020-10-04 20:17:41 -0500

    Updating .gitignore for Visual Studio Code.

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Logging.Serilog;
-using Avalonia.ReactiveUI;
+﻿namespace Macabresoft.Tuner.Desktop {
 
-namespace Macabresoft.Tuner.Desktop {
-    class Program {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    using Avalonia;
+    using Avalonia.Logging.Serilog;
+    using Avalonia.ReactiveUI;
+
+    internal class Program {
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
@@ -18,5 +12,11 @@ namespace Macabresoft.Tuner.Desktop {
                 .UsePlatformDetect()
                 .LogToDebug()
                 .UseReactiveUI();
+
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
     }
 }
