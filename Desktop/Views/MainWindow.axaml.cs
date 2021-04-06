1590bcee1c59d15b4f6d66a308b3fda8ad3cbc7f
Brett Story <brettmstory@gmail.com>
2021-04-05 19:26:33 -0500

    Cleaning up solution and getting window size working on mac.

--- Desktop/Views/MainWindow.axaml.cs
+++ Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Macabresoft.Zvukosti.Desktop.Views {
+﻿using Avalonia;
+
+namespace Macabresoft.Zvukosti.Desktop.Views {
 
     using Avalonia.Controls;
     using Avalonia.Interactivity;
@@ -9,6 +11,15 @@
 
         public MainWindow() {
             InitializeComponent();
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e) {
+            base.OnAttachedToVisualTree(e);
+
+            // This can't be set in the window's XAML because of an issue with
+            // AvaloniaUI not sizing content correctly unless the rest of the
+            // window is loaded first.
+            this.SizeToContent = SizeToContent.Height;
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e) {
